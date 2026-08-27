1929ba181589e60aea1fe0c2d3b66cd5208e41fa
jysongpro <jysongpro@gmail.com>
2026-08-27 09:50:49 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,12 +3,11 @@
 // Firestore Database(테스트 모드 또는 규칙 설정)를 반드시 활성화해야 합니다.
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-    apiKey: "AIzaSyCXcQh1e7ZIyXQe0WHi3zzI-IiU69VHpx8",
-    authDomain: "kopoai.firebaseapp.com",
-    projectId: "kopoai",
-    storageBucket: "kopoai.firebasestorage.app",
-    messagingSenderId: "741213898877",
-    appId: "1:741213898877:web:7368d5dc031b3964754914",
-    measurementId: "G-905T1MD57K"
-
+  apiKey: "AIzaSyCXcQh1e7ZIyXQe0WHi3zzI-IiU69VHpx8",
+  authDomain: "kopoai.firebaseapp.com",
+  projectId: "kopoai",
+  storageBucket: "kopoai.firebasestorage.app",
+  messagingSenderId: "741213898877",
+  appId: "1:741213898877:web:7368d5dc031b3964754914",
+  measurementId: "G-905T1MD57K"
 };
